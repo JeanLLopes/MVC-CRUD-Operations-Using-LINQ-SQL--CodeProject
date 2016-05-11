a92f48fc1aa96b62a210ed465190f6dbe788cecc
JeanLLopes <jean.llopes@outlook.com>
2016-05-10 22:01:03 -0300

    Add Model Add Style Add Views

---
+++ MVC-CRUD.Web/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CRUD.Web.Models
+{
+    public class User
+    {
+        public Int32 UserId { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String Address { get; set; }
+        public String PhoneNo { get; set; }
+        public String Company { get; set; }
+        public String Designation { get; set; }
+    }
+}
